3085fa6331eaf120bd96693bc5f197b51cbc25ce
ms0000001 <alstn830@gmail.com>
2022-12-29 10:00:17 +0900

    test

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Com1=0; int Com2=0; int MyCard=0;
+            TrumpCard_ tc = new TrumpCard_();
+            tc.SetupTrumpCards();
+            int player = 10000;
+            while(true)
+            {
+
+                Com1 = tc.ReRollCard_();
+                Com2 = tc.ReRollCard_();
+
+
+                // { 베팅
+                int betP = default;
+                Console.WriteLine("현재 포인트 : {0}", player);
+                while (true)
+                {
+                Console.Write("배팅 포인트 입력 : ");
+                int.TryParse(Console.ReadLine(), out betP);
+
+                if(betP<0||betP>player)
+                {
+                    Console.WriteLine("입력 값을 확인해주세요");
+                }
+                else { break; }
+                }
+
+                player = player - betP;
+
+
+                // } 베팅
+
+                // { 플레이어 카드 뽑기
+                MyCard = tc.ReRollCard_();
+                // } 플레이어 카드 뽑기
+
+                // { 라운드 결과 체크
+                // 변수 두개 선언해서
+                int bigger, smaller;
+                // 큰수 작은수 넣어준 다음에
+                if (Com1 > Com2)
+                {
+                    bigger = Com1;
+                    smaller = Com2;
+                }
+                else
+                {
+                    smaller = Com1;
+                    bigger = Com2;
+                }
+                // 작은 수 < 플레이어 < 큰수 이거 조건 확인
+                if (smaller<MyCard&&MyCard<bigger) 
+                {
+                    Console.WriteLine("승");
+                    player = (betP * 2) + player;
+                }
+                else
+                {
+                    Console.WriteLine("패");
+                }
+                // } 라운드 결과 체크
+                Console.WriteLine("남은 포인트 : {0}", player);
+                
+                if (player <= 0 || player >= 100000)
+                {
+                    break;
+                }
+                
+                //빠져나갈거
+                Console.WriteLine("\n\nPress any key");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/TrumpCard.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class TrumpCard_
+    {
+        private int[] trumpCardSet;     //카드세트
+        private string[] trumpCardMark;
+
+        public void SetupTrumpCards()
+        {
+            trumpCardSet = new int[52];
+            for (int i = 0; i < trumpCardSet.Length; i++)
+            {
+                trumpCardSet[i] = i + 1;
+            } // 카드를 셋업하는 루프
+
+            trumpCardMark = new string[4] {"♥","♠","◈","♣"};
+        }
+        public void ShuffleCards()
+        {
+            ShuffleCards(200);
+        } //Shufflecards
+
+        public int ReRollCard_()
+        {
+            ShuffleCards();
+            return Rollcard_();
+        }
+        
+        public int Rollcard_()
+        {
+            int card = trumpCardSet[0];
+            string cardMark = trumpCardMark[(card-1) / 13];
+            //int cardNumber = (int)Math.Ceiling(card % 13.1);
+            string cardNumber = Math.Ceiling(card % 13.1).ToString();
+
+            switch(cardNumber)
+            {
+                case "11":
+                    cardNumber= "J";
+                    break;
+                case "12":
+                    cardNumber = "Q";
+                    break;
+                case "13":
+                    cardNumber = "K";
+                    break;
+                default:
+                    break;
+            }
+
+            Console.WriteLine("내가 뽑은 카드는 {0}{1}입니다.",cardMark, cardNumber);
+            Console.WriteLine(" -------");
+            Console.WriteLine("|{0}{1}\t|", cardMark, cardNumber);
+            Console.WriteLine("|       |");
+            Console.WriteLine("|       |");
+            Console.WriteLine("|       |");
+            Console.WriteLine("|       |");
+            Console.WriteLine(" -------");
+
+            return (int)Math.Ceiling(card % 13.1);
+        } //Rollcard
+
+        public void PrintCardSet()
+        {
+            foreach (int card in trumpCardSet)
+            {
+                Console.Write(" {0} ", card);
+            }
+        } //PrintCardSet
+        private int[] ShuffleOnce(int[] intArray)
+        {
+            Random random= new Random();
+            int sourindex = random.Next(0, intArray.Length);
+            int destindex = random.Next(0, intArray.Length);
+
+            int tempVariable = intArray[sourindex];
+            intArray[sourindex] = intArray[destindex];
+            intArray[destindex] = tempVariable;
+
+            return intArray;
+        } //ShuffleOnce
+
+        private void ShuffleCards(int howManyLoop)
+        {
+            for (int i = 0; i < howManyLoop; i++)
+            {
+                trumpCardSet = ShuffleOnce(trumpCardSet);
+            }
+        } //ShuffleCards
+
+    }
+}
